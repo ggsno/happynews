5dde430c48ead2526edf94b15aa965084d788b9e
ggsno <ogs0518@gmail.com>
2023-08-01 13:08:56 +0900

    fix: route handler 삭제 - server component에서 불러올 수 없다고 함

--- src/api/article.ts
+++ src/api/article.ts
@@ -1,13 +1,15 @@
 import { ArticleProps } from "@/components/Article";
+import { NextResponse } from "next/server";
+import dummy from "./dummyArticles.json";
 
 export const getArticles = async (id?: string): Promise<ArticleProps[]> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_HOSTNAME}/api/article${id ? `?id=${id}` : ""}`
-  );
+  // const res = await fetch(`${BACKEND_URL}/api/article${id ? `?id=${id}` : ""}`);
 
-  if (!res.ok) {
-    throw new Error(`Faild to fetch data. error code: ${res.status}`);
-  }
+  // if (!res.ok) {
+  //   throw new Error(`Faild to fetch data. error code: ${res.status}`);
+  // }
 
-  return res.json();
+  // return res.json();
+
+  return NextResponse.json(dummy).json();
 };
