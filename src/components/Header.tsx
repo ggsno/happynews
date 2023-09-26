cd26440596910ea06855aeeb921f5734214df3cc
ggsno <ogs0518@gmail.com>
2023-09-26 13:41:30 +0900

    design: header 로고 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,18 @@
-"use client";
+import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 export default function Header() {
-  const pathname = usePathname();
-
   return (
     <div className="fixed top-0 bg-white w-screen h-[var(--header-height)] flex justify-center">
-      <div className="flex justify-between text-lg p-5 w-[var(--content-width)]">
-        <h1>행복한 소식</h1>
-        <Link
-          href={"/overview"}
-          className={`${pathname === "/overview" ? "hidden" : ""}`}
-        >
-          전체 보기
+      <div className="flex justify-between items-center px-4 text-lg w-[var(--content-width)]">
+        <Link href={"/overview"}>
+          <Image
+            src="/happiest_sun.gif"
+            width={60}
+            height={60}
+            alt="행복한 소식"
+            className="transform hover:scale-[10] duration-[20000ms] delay-[3000ms] origin-top-left"
+          />
         </Link>
       </div>
     </div>
