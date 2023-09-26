741ded33d7f19c572f8a065e71a6c9da859c5131
ggsno <ogs0518@gmail.com>
2023-09-26 19:02:53 +0900

    fix: GET articles api 캐시 무효화 - 랜덤으로 받아와야하기 때문에 캐시를 무효화

--- src/api/article.ts
+++ src/api/article.ts
@@ -2,7 +2,8 @@ import { ArticleProps } from "@/components/Article";
 
 export const getArticles = async (id?: string): Promise<ArticleProps[]> => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_URL}/articles${id ? `/${id}` : ""}`
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/articles${id ? `/${id}` : ""}`,
+    { cache: "no-store" }
   );
 
   if (!res.ok) {
