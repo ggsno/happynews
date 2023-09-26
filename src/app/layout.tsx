8aff7879acbb1108ac4da6b89dfba5631ea3e504
ggsno <ogs0518@gmail.com>
2023-09-26 15:07:57 +0900

    chores: og 태그 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,10 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "행복한 소식",
   description: "행복한 소식을 모아 전해드립니다.",
+  openGraph: {
+    images: "/happiest_sun.gif",
+  },
+  icons: "/happiest_sun.gif",
 };
 
 export default function RootLayout({
