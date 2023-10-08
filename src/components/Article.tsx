a4b7006b5c48dfbc54407598cd0af02f20ff42f7
ggsno <ogs0518@gmail.com>
2023-10-08 14:45:58 +0900

    style: 원문 보러가기 위치 변경 - 텍스트 많고 화면 작을 경우에 원문 보러가기 클릭이 어려워 위치 변경

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -14,16 +14,21 @@ export default function Article(props: ArticleProps) {
         ref={setTarget}
         className="rounded-lg h-full overflow-hidden border"
       >
-        <div className={`h-1/2 bg-yellow-300`} />
+        <div className={`h-1/2 bg-yellow-300`}>
+          <div className="flex justify-end h-full">
+            <a
+              href={link}
+              target="_blank"
+              className="self-end p-2 opacity-70 text-sm"
+            >
+              원문 보러가기 &gt;
+            </a>
+          </div>
+        </div>
         <div className="p-5 h-1/2 flex flex-col justify-between">
           <div>
             <h1 className="font-bold text-lg">{title}</h1>
             <div className="my-2">{summary}</div>
-          </div>
-          <div className="text-right">
-            <a href={link} target="_blank">
-              원문 보러가기 &gt;
-            </a>
           </div>
         </div>
       </article>
